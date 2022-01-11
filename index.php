5d09616c881d40ea1e9e932106e844736a891ed0
Adam Percival <Percival.Adam@student.greenriver.edu>
2022-01-11 17:22:23 -0500

    Added php block for name,date,link,and assaignment

--- index.php
+++ index.php
@@ -14,5 +14,13 @@
 </head>
 <body>
     <h1>Welcome to Pair Programming 2</h1>
+    <?php
+    echo '<h2>Adam Percival<h2>';
+    echo '<h2>1/11/2022<h2>';
+    echo '<h2>https://apercival.greenriverdev.com/328/PP2/index.php<h2>';
+    echo '<h2>Pair Program 2<h2>';
+
+    ?>
+
 </body>
 </html>
